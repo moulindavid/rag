86c7405ea5a2948cd2add0a9630c7b0d821ea197
David Moulin <moulin.david@live.fr>
2026-03-19 12:40:27 +0100

    feat: parser for txt and pdf files

---
+++ internal/parser/parser.go
@@ -0,0 +1,48 @@
+package parser
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"path/filepath"
+	"strings"
+
+	"github.com/dslipak/pdf"
+)
+
+func Parse(filename string, reader io.Reader) (string, error) {
+	extension := filepath.Ext(filename)
+	switch extension {
+	case ".txt":
+		file, err := io.ReadAll(reader)
+		if err != nil {
+			return "", err
+		}
+		return strings.TrimSpace(string(file)), nil
+
+	case ".pdf":
+		data, err := io.ReadAll(reader)
+		if err != nil {
+			return "", fmt.Errorf("reading pdf: %w", err)
+		}
+
+		r, err := pdf.NewReader(bytes.NewReader(data), int64(len(data)))
+		if err != nil {
+			return "", fmt.Errorf("parsing pdf: %w", err)
+		}
+
+		var sb strings.Builder
+		for i := 1; i <= r.NumPage(); i++ {
+			page := r.Page(i)
+			text, err := page.GetPlainText(nil)
+			if err != nil {
+				return "", fmt.Errorf("extracting text from page %d: %w", i, err)
+			}
+			sb.WriteString(text)
+		}
+		return strings.TrimSpace(sb.String()), nil
+
+	default:
+		return "", fmt.Errorf("unsupported file extension: %s", extension)
+	}
+}
